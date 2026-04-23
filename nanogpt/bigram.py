fe59b6f84051b4125f0e9ad83d0d4ca2a416f907
Zac Valant <zac23.v@gmail.com>
2026-04-22 21:57:13 -0500

    create layer normalization and attentention blocks & feedforward

--- nanogpt/bigram.py
+++ nanogpt/bigram.py
@@ -5,12 +5,14 @@ from torch.nn import functional as F
 #hyperparameters
 batch_size = 32
 block_size = 8
-max_iters = 3000
-eval_interval = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
+n_layer = 4
+n_head = 4
 
 
 torch.manual_seed(1337)
@@ -59,18 +61,45 @@ def estimate_loss():
     model.train()
     return out
 
-
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd)
+        )
+    def forward(self, x):
+        return self.net(x)
+    
+class Block(nn.Module):
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa_head = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+    def forward(self, x):
+        x = x + self.sa_head(self.ln1(x))
+        x =x +  self.ffwd(self.ln2(x))
+        return x
+    
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head) for _ in range(n_layer)])
         self.lm_head = nn.Linear(n_embd, vocab_size)
+        self.ln_f = nn.LayerNorm(n_embd)
     def forward(self, idx, targets=None):
         B,T = idx.shape
         tok_emb = self.token_embedding_table(idx)
         pos_embedding = self.position_embedding_table(torch.arange(T, device=device))
         x = tok_emb + pos_embedding
+        x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x)
         if targets is None:
             loss = None
@@ -91,10 +120,52 @@ class BigramLanguageModel(nn.Module):
             idx_next = torch.multinomial(probs, num_samples=1) # (B,1)
             idx = torch.cat((idx, idx_next), dim=1) # append to the running sequence (B,T+1)
         return idx
+class LayerNorm:
+    def __init__(self, dim, eps = 1e-5):
+        self.eps = eps
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
 
 
+    def __call__(self, x):
+        xmean = x.mean(1, keepdim=True)
+        xvar = x.var(1, keepdim=True, unbiased=True)
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps)#normalize and make sure we have value under sqrt
+        self.out = self.gamma * xhat + self.beta
+        #update running stats
+        return self.out
+    def parameters(self):
+        return [self.gamma, self.beta]
+    
 
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    def forward(self, x):
+        B,T,C = x.shape
+        q = self.query(x)
+        k = self.key(x)
+        v = self.value(x)
 
+        wei = q @ k.transpose(-2,-1) * (C ** -0.5)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        out = wei @ v
+        return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+    def forward(self,x):
+        out =  torch.cat([h(x) for h in self.heads], dim = -1)
+        out = self.proj(out)
+        return out
 model = BigramLanguageModel()
 m = model.to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
@@ -111,4 +182,4 @@ for iter in range(max_iters):
     optimizer.step()
 
 context = torch.zeros((1,1), dtype=torch.long, device=device)
-print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
+print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=500)[0].tolist()))
